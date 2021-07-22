fdad849481ea20ca1666325959db01aea1bcf289
gloriankosi <kosig@acm.org>
2021-07-21 23:55:40 -0400

    comments for list-transform.cc

--- list-transform.cc
+++ list-transform.cc
@@ -1,3 +1,15 @@
+/**
+ *
+ * @file list-transform.cc
+ * @author Glorian Kosi
+ * @brief
+ * Given an integer array, keep the order while reassinging minimizes values
+ * Example:
+ * vec = {1 , 5 , 6 , 7, 1}
+ * Should turn into:
+ * {1 , 2 , 3 , 4 , 1}
+ *
+ */
 #include <iostream>
 #include <set>
 #include <unordered_map>
@@ -7,37 +19,41 @@ using namespace std;
 vector<int> reassignPriorities(vector<int>);
 
 int main(int argc, char* argv[]) {
-    vector<int> priorities = {5, 3, 4, 1, 9};
-    auto v = reassignPriorities(priorities);
-    for (auto& a : v) {
-        cout << a << "\n";
-    }
-    return 0;
+  vector<int> priorities = {1, 3, 4, 1, 9};
+  auto v = reassignPriorities(priorities);
+  for (auto& a : v) {
+    cout << a << "\n";
+  }
+  return 0;
 }
 
 vector<int> reassignPriorities(vector<int> priorities) {
-    unordered_map<int, int> priorityMap;
-    set<int> prioritySet;
-    auto prioritiesCopy = priorities;
-    sort(prioritiesCopy.begin(), prioritiesCopy.end());
-    for (auto& a : prioritiesCopy) {
-        prioritySet.insert(a);
-        priorityMap[a] = 0;
-    }
+  unordered_map<int, int> priorityMap;
+  set<int> prioritySet;  // gets rid of duplicates and orders the vector
+  for (auto& a : priorities) {
+    prioritySet.insert(a);
+    priorityMap[a] = 0;
+  }
 
-    auto priorityMapSize = priorityMap.size();
+  auto priorityMapSize =
+      priorityMap
+          .size();  // starting size, this is also the new value of the max int
 
-    while (!prioritySet.empty()) {
-        auto max = (prioritySet.end());
-        --max;
-        priorityMap[*(max)] = priorityMapSize;
-        priorityMapSize = priorityMapSize - 1;
-        auto i = prioritySet.end();
-        --i;
-        prioritySet.erase(i);
-    }
-    for (auto& a : priorities) {
-        a = priorityMap[a];
-    }
-    return priorities;
+  while (!prioritySet.empty()) {
+    auto max = (prioritySet.end());  // get max element
+    --max;                           // end() - 1
+    priorityMap[*(max)] =
+        priorityMapSize;  // add new value to the map with the original as key
+    priorityMapSize = priorityMapSize - 1;  // decrement size
+    auto i = prioritySet.end();
+    --i;  // end() -1
+    prioritySet.erase(
+        i);  // delete last element in set, i.e, largest (automatically ordered)
+  }
+
+  for (auto& a : priorities) {
+    a = priorityMap[a];  // assign new values to original unsorted vector
+  }
+
+  return priorities;
 }
