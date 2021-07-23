6b6aa1cc0cd43032a6add16889cedff7c8c4c81d
gloriankosi <kosig@acm.org>
2021-07-22 20:47:26 -0400

    brute force remove duplicates in place algorithm

---
+++ remove-duplicates-in-place.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void removeDuplicates(vector<int> &);
+
+int main(int argc, char *argv[]) {
+  vector<int> nums = {1, 1, 2, 2, 3, 4, 5, 7, 7, 7};
+  removeDuplicates(nums);
+  for (auto &a : nums) {
+    cout << a << "\n";
+  }
+  return 0;
+}
+
+// O(n^2) solution
+void removeDuplicates(vector<int> &nums) {
+  for (auto i = 0; i < nums.size(); i++) {
+    if ((i + 1) <= nums.size()) {
+      if (nums[i] == nums[i + 1]) {
+        nums.erase(nums.begin() + i);
+      }
+    }
+  }
+  return;
+}
